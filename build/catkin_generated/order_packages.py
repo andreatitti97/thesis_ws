5b7064c5e1a3bab858c1d8eef357d67a0dbf88f3
andreatitti97 <andrea.tiranti97@gmail.com>
2021-02-19 19:45:31 +0100

    first_commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/andrea/thesis_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/andrea/thesis_ws/devel;/home/andrea/catkin_ws/devel;/opt/ros/kinetic'.split(';') if '/home/andrea/thesis_ws/devel;/home/andrea/catkin_ws/devel;/opt/ros/kinetic' != '' else []
